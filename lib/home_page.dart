5a4dc540fb53777a56b0b4af2cd65d7d2da6ba8e
Peeyush <69499613+PeeyushSachan@users.noreply.github.com>
2024-12-24 10:02:09 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Media Query"),
+      ),
+
+      body: Center(
+        child: Container(
+        
+        width:  100,
+
+        height: 100,
+          child: ,
+        ),
+      ),
+    );
+  }
+}
