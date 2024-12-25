9b7c7b9d87e19c14100ac49f4fcf58cef0ca91a1
Peeyush <69499613+PeeyushSachan@users.noreply.github.com>
2024-12-25 16:25:34 +0530

    completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatelessWidget {
   MediaQueryData? mqData;
@@ -18,13 +18,49 @@ class HomePage extends StatelessWidget {
                 width: mqData!.size.width * .6,
                 height: mqData!.size.height * 0.8,
                 color: Colors.orange,
+                child: OrientationBuilder(builder: (_, orientation) {
+                  log("(orientation width : ${orientation}, orientation height: ${orientation})");
+
+                  return Orientation.portrait == orientation
+                      ? Center(
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.red,
+                          ),
+                        )
+                      : Center(
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: const Color.fromARGB(255, 31, 14, 13),
+                          ),
+                        );
+                }),
               ),
             )
           : Center(
               child: Container(
                 width: mqData!.size.width * .6,
                 height: mqData!.size.height * 0.8,
-                color: Colors.black,
+                color: Colors.grey,
+                child: LayoutBuilder(builder: (_, Constraints) {
+                  log("(Constraints width : ${Constraints.maxWidth})");
+                  return Center(
+                    child: Container(
+                        width: Constraints.maxWidth * 0.4,
+                        height: Constraints.maxHeight * 0.1,
+                        color: Colors.red,
+                        child: FittedBox(
+
+                          fit: BoxFit.fill,
+                          child: Text(
+                            "ABCD Ok",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        )),
+                  );
+                }),
               ),
             ),
     );
